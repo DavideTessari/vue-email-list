0e4e84216f2034bd062a43384159451aa7c9fed4
DavideTessari <davtex@gmail.com>
2024-04-09 15:33:05 +0200

    feat: make 10 email

--- js/script.js
+++ js/script.js
@@ -11,14 +11,29 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            userEmail: null
+            userEmails: []
         };
     },
     mounted() {
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then((response) => {
-            const serverData = response.data;
-            this.userEmail = serverData.response;
-        })
+        // Inizializza un array vuoto per memorizzare le email
+        const emails = [];
+
+        // Ciclo per ottenere 10 email diverse
+        for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) => {
+                    const serverData = response.data;
+                    const email = serverData.response;
+                    emails.push(email);
+
+                    // Quando abbiamo tutte e 10 le email, assegnale a userEmails
+                    if (emails.length === 10) {
+                        this.userEmails = emails;
+                    }
+                })
+        }
     }
 }).mount('#app');
+
+
+
