72d8bb90c2cb1f2bcb3cfa1c4ab15bf8f0d39932
DavideTessari <davtex@gmail.com>
2024-04-09 15:18:10 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Descrizione:
+// Attraverso l'apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
+// Bonus
+// Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            userEmail: null
+        };
+    },
+    mounted() {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((response) => {
+            const serverData = response.data;
+            this.userEmail = serverData.response;
+        })
+    }
+}).mount('#app');
